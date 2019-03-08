08f978c2c21dd8abca481dea126135d6f323330b
magno Oliveira <magnose2011@gmail.com>
2019-03-08 14:33:40 -0400

    Initial Commit

---
+++ guia clima/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  guia clima
+//
+//  Created by magno Oliveira on 08/03/19.
+//  Copyright © 2019 Embrapa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
